a4d5e661c43199e31955d54e58cae3d2ca9f6171
Tanvir Waseer <twaseer@protonmail.com>
2023-10-02 12:43:24 -0700

    setting up the list of data

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.affirmations.data.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textview)
+
+        textView.text = DataSource().loadAffirmations().size.toString()
     }
 }
